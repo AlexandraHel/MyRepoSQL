88ecb0e4c57b1011136a3274c3bd14eae44d13e9
Alexandra Helmersson <alexandra.helmersso@hotmail.com>
2025-04-25 10:08:42 +0200

    Friend funkar

--- DbContext/Migrations/SqlServerDbContext/20250425071513_miInitial.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20250425071513_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(MainDbContext.SqlServerDbContext))]
-    [Migration("20250423162139_miInitial")]
+    [Migration("20250425071513_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />
